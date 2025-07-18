0e0a38a8638d48278c6b87f405cf714e57b6c8d6
Kassandor <webmasterdracarys@gmail.com>
2025-07-18 15:40:09 +0300

    Перенес weatherforcast в контроллеры

--- src/Server/TutorCabinet.Api/Program.cs
+++ src/Server/TutorCabinet.Api/Program.cs
@@ -8,6 +8,7 @@ public static class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
 
         if (builder.Environment.IsDevelopment())
         {
@@ -18,6 +19,9 @@ public static class Program
         }
 
         var app = builder.Build();
+        app.MapControllers();
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -26,29 +30,6 @@ public static class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
 
         app.Run();
     }
--- src/Server/TutorCabinet.Api/WeatherForecast.cs
+++ src/Server/TutorCabinet.Api/WeatherForecast.cs
@@ -1,12 +1,10 @@
 namespace TutorCabinet.Api;
 
-public class WeatherForecast
+public readonly record struct WeatherForecast
 {
-    public DateOnly Date { get; set; }
+    public DateOnly Date { get; init; }
 
-    public int TemperatureC { get; set; }
+    public int TemperatureC { get; init; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string? Summary { get; set; }
+    public string? Summary { get; init; }
 }
